6461799daea136832b61d30c1176a07aea5bb60b
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2014-11-26 15:47:23 -0500

    Fix non-build deps.

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection 'multi)
 
-(define deps '("base" "typed-racket-lib"))
+(define deps '("base" "typed-racket-lib" "typed-racket-more"))
 
-(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib" "typed-racket-more"))
+(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib"))
 
