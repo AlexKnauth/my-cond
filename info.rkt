dfc969924a5cfbb7250acb6e301167bc6684f75f
AlexKnauth <alexander@knauth.org>
2017-01-18 17:38:25 +0100

    only depend on sweet-exp-lib in deps

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection 'multi)
 
-(define deps '("base" "typed-racket-lib" "typed-racket-more" "sweet-exp"))
+(define deps '("base" "typed-racket-lib" "typed-racket-more" "sweet-exp-lib"))
 
-(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib"))
+(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib" "sweet-exp"))
 
